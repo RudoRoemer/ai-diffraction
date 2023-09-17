40a4620bf57fd1a7e9602c87510816c7448d893d
unknown <xexpozure@gmail.com>
2023-09-18 00:00:43 +0100

    script updates

--- scripts/0.sh
+++ scripts/0.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --partition=compute
-#SBATCH --time=30:00:00
+#SBATCH --partition=gpu
+#SBATCH --time=10:00:00
 
 module restore ai
 
-srun python src/train.py --dataroot ~/FDP --split ./data/splits/0 --gpu_ids -1
+srun python src/train.py --dataroot ~/FDP --split ./data/splits/0 --gpu_ids 0
--- scripts/4.sh
+++ scripts/4.sh
@@ -3,10 +3,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --partition=compute
-#SBATCH --time=30:00:00
+#SBATCH --partition=gpu
+#SBATCH --time=10:00:00
 
 module restore ai
 
-srun python src/train.py --dataroot ~/FDP --split ./data/splits/4 --gpu_ids -1
+srun python src/train.py --dataroot ~/FDP --split ./data/splits/4 --gpu_ids 0
 
