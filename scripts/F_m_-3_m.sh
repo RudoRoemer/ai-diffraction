23e60c0db03b33fae54058f289aafdcf1e086cf0
unknown <xexpozure@gmail.com>
2023-09-18 00:25:15 +0100

    update scripts

--- scripts/F_m_-3_m.sh
+++ scripts/F_m_-3_m.sh
@@ -3,11 +3,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=42
 #SBATCH --mem-per-cpu=3850
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu
+#SBATCH --partition=compute
 #SBATCH --time=10:00:00
 
 module restore ai
 
-srun python src/train.py --dataroot ~/FDP --split ./data/splits/F_m_-3_m --gpu_ids 0 --input CD --name seedF_m_-3_m_CD
+srun python src/train.py --dataroot ~/FDP --split ./data/splits/F_m_-3_m --gpu_ids -1 --input CD --name seedF_m_-3_m_CD
 
