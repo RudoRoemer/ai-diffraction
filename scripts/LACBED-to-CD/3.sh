5bf9594eec2172a54176cc274cd1651fe8b873fc
unknown <xexpozure@gmail.com>
2023-09-18 23:02:15 +0100

    scripts update

---
+++ scripts/LACBED-to-CD/3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=42
+#SBATCH --mem-per-cpu=3850
+#SBATCH --partition=compute
+#SBATCH --time=10:00:00
+
+module restore ai
+
+srun python train.py --dataroot ~/FDP --split ./data/splits/3 --gpu_ids -1 --input LACBED --name seed3_CD --no_vgg_loss --continue_train
+
