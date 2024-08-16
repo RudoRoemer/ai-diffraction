7cd879aaa114e50fa4344b2d1c980ce1035d1a1f
unknown <joe@wephy.com>
2024-08-16 19:11:02 +0100

    update scripts

--- scripts/getting_results.sh
+++ scripts/getting_results.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-for PHASE in train test val
-do
-    for SPLIT in 0 1 2 3 4 5 6 7 8 9
-    do
-        for EPOCH in 10
-        do
-            python test.py --dataroot datasets/FDP --gpu_ids 0 --split data/FDP_splits/random/${SPLIT} --input structure --name pattern_random${SPLIT} --how_many 99999 --phase ${PHASE} --which_epoch ${EPOCH}
+for PHASE in train test val; do
+    for SPLIT in 0 1 2 3 4 5 6 7 8 9; do
+        for EPOCH in 10; do
+            python test.py --dataroot "" --gpu_ids 0 --split ""${SPLIT} --input structure --how_many 99999 --phase ${PHASE} --which_epoch ${EPOCH}
         done
     done
 done
